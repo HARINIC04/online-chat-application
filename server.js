2c76775520e22ca74b80774d9a5143be857586d7
HARI <kit27.ad23@gmail.com>
2025-08-25 21:35:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected: ' + socket.id);
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // broadcast to all users
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected: ' + socket.id);
+  });
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
